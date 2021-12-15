96797996bdb6734e263970f928038f7346e447a3
Francesco Piccinelli Casagrande <francesco.piccinelli@gmail.com>
2021-12-15 20:12:47 +0100

    Update sitemap_scraping.R

--- sitemap_scraping.R
+++ sitemap_scraping.R
@@ -14,8 +14,7 @@ iso_3 = iso_3 %>% select(ISO3)
 iso_3 = iso_3$ISO3
 oid_data = subset(oid_data, iso_code %in% iso_3)
 oid_rest = subset(oid_data, !(iso_code %in% iso_3))
-cases_latest = oid_data %>% select(location, iso_code, new_cases) %>% 
-filter(date = max(date))
+cases_latest = oid_data %>% select(location, date, iso_code, new_cases) %>% filter(date = max(date))
 rename(speed_data_latest, iso_a3 = iso_code)
 write_csv(cases_latest, 'new_cases_latest.csv')
 
